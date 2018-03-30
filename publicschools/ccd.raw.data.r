8590e1b5be5cdd0c5eb8ecd98023ff7694bf2753
Samantha Rhoads <skr2150@columbia.edu>
2018-03-30 12:33:50 -0400

    collect raw public school ccd data from the nces

---
+++ publicschools/ccd.raw.data.r
@@ -0,0 +1,20 @@
+# public school data
+
+library(haven) # use to import files
+? read_sas() # use this with regular file path inside to get document as data
+
+# 1999-2000 public schools ccd
+### https://nces.ed.gov/ccd/data/zip/sc991bai.zip # data  
+### https://nces.ed.gov/ccd/data/zip/sc991bkn.zip # data  
+### https://nces.ed.gov/ccd/data/zip/sc991bow.zip # data 
+
+# 2000-2001 public schools ccd
+### https://nces.ed.gov/ccd/data/zip/sc001aai.zip # data 
+### https://nces.ed.gov/ccd/data/zip/sc001akn.zip # data  
+### https://nces.ed.gov/ccd/data/zip/sc001aow.zip # data 
+
+# 2009-2010 public schools ccd
+### https://nces.ed.gov/ccd/data/zip/sc092a_sas.zip # data 
+
+# 2010-2011 public schools ccd
+### https://nces.ed.gov/ccd/Data/zip/sc102a_sas7bdat.zip # data
